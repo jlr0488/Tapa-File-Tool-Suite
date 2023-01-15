db57faa1ac27ec0dab1e924c4be5ca20faa0be2c
Joshua_Robbins <Joshua_Robbins@Dell.com>
2023-01-15 13:16:26 -0600

    fixing a bug in the file processing

--- K1_Parser_v1/Tool_Logic/K1_Processor.cs
+++ K1_Parser_v1/Tool_Logic/K1_Processor.cs
@@ -1,9 +1,11 @@
 ﻿using iTextSharp.text;
 using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Windows;
 
 namespace K1_Parser_v1.Tool_Logic
 {
@@ -14,6 +16,10 @@ namespace K1_Parser_v1.Tool_Logic
             public string Name;
 
             public iTextSharp.text.pdf.PdfDocument K1;
+
+            public int StartingPage;
+
+            public int EndPage;
         }
 
         public string OutputDirectory;
@@ -29,7 +35,7 @@ namespace K1_Parser_v1.Tool_Logic
 
         public void LoadPDF(string filePath)
         {
-            int interval = 4;
+            int interval = 0;
             int pageNameSuffix = 0;
 
             StringBuilder sb = new StringBuilder();
@@ -37,63 +43,106 @@ namespace K1_Parser_v1.Tool_Logic
             PdfReader reader1 = new PdfReader(filePath);
 
 
-            for (int i = 1; i <= reader1.NumberOfPages; i = i + 4)
+            for (int i = 1; i <= reader1.NumberOfPages; i++)
             {
-                string page = "";
-
-                page = PdfTextExtractor.GetTextFromPage(reader1, i);
-                string[] lines = page.Split('\n');
-
-                // get the company name from the first page
-                if (i == 1)
+                try
                 {
+                    string page = "";
+
+                    page = PdfTextExtractor.GetTextFromPage(reader1, i);
+                    string[] lines = page.Split('\n');
+
+                    // get the company name from the first page
+                    if (i == 1)
+                    {
+                        foreach (string line in lines)
+                        {
+                            if (line.Contains("Income of"))
+                            {
+                                var newLine = line.Replace("Income of ", "");
+
+                                int index = newLine.IndexOf(" LLC");
+                                string result = newLine.Substring(0, index);
+
+                                CompanyName = result;
+
+                                break;
+                            }
+                        }
+                    }
+
+                    // get each investor's name
                     foreach (string line in lines)
                     {
-                        if (line.Contains("Income of"))
+                        if (line.Contains("Dear"))
                         {
-                            var newLine = line.Replace("Income of ", "");
+                            var newLine = line.Replace("Dear ", "");
 
-                            int index = newLine.IndexOf(" LLC");
-                            string result = newLine.Substring(0, index);
+                            if (newLine.Contains(","))
+                            {
+                                newLine = newLine.Replace(",", "");
+                            }
 
-                            CompanyName = result;
+                            K1_List.Add(new Investor_Info() { Name = newLine, StartingPage = i }); ;
 
                             break;
                         }
                     }
                 }
-
-                // get each investor's name
-                foreach (string line in lines)
+                catch (Exception ex)
                 {
-                    if (line.Contains("Dear"))
-                    {
-                        var newLine = line.Replace("Dear ", "");
-
-                        K1_List.Add(new Investor_Info() { Name = newLine }); ;
-
-                        break;
-                    }
+                    // if any exception occurs, we will show the error and continue processing
+                    MessageBox.Show(ex.Message);
                 }
             }
 
             // create separate K-1 pdf documents for each investor 
             PdfReader reader = new PdfReader(filePath); // remove
             int currentInvestor = 0;
-            for (int pageNumber = 1; pageNumber <= reader.NumberOfPages; pageNumber += interval)
+            for (int pageNumber = 1; pageNumber <= reader.NumberOfPages;)
             {
-                pageNameSuffix++;
-                string newPdfFileName = System.IO.Path.Combine(OutputDirectory, CompanyName
+                //pageNameSuffix++;
+
+                // make sure that there is at least one more investor in the list before processing
+                if(K1_List.Count > currentInvestor + 1)
+                {
+                    interval = K1_List[currentInvestor + 1].StartingPage - K1_List[currentInvestor].StartingPage;
+
+                    string newPdfFileName = System.IO.Path.Combine(OutputDirectory, CompanyName
                     + "_K1_" + K1_List[currentInvestor].Name + ".pdf");
 
-                if (File.Exists(newPdfFileName))
+                    if (File.Exists(newPdfFileName))
+                    {
+                        newPdfFileName = System.IO.Path.Combine(OutputDirectory, CompanyName
+                        + "_K1_" + K1_List[currentInvestor].Name + " (2)" + ".pdf");
+                    }
+
+                    SplitAndSaveInterval(filePath, pageNumber, interval, newPdfFileName);
+                    // get the page number where the next investor documents begin
+                    currentInvestor++;
+                    pageNumber = K1_List[currentInvestor].StartingPage;
+                }
+                else
                 {
-                    newPdfFileName = System.IO.Path.Combine(OutputDirectory, CompanyName
-                    + "_K1_" + K1_List[currentInvestor].Name + " (2)" + ".pdf");
+                    // if there are no more investors, process the current investor until the end of the document
+                    interval = reader.NumberOfPages - K1_List[currentInvestor].StartingPage + 1;
+
+                    string newPdfFileName = System.IO.Path.Combine(OutputDirectory, CompanyName
+                    + "_K1_" + K1_List[currentInvestor].Name + ".pdf");
+
+                    if (File.Exists(newPdfFileName))
+                    {
+                        newPdfFileName = System.IO.Path.Combine(OutputDirectory, CompanyName
+                        + "_K1_" + K1_List[currentInvestor].Name + " (2)" + ".pdf");
+                    }
+
+                    SplitAndSaveInterval(filePath, pageNumber, interval, newPdfFileName);
+
+                    // since we just processed the last investor, we should go ahead and break out of the for loop
+                    break;
                 }
 
-                SplitAndSaveInterval(filePath, pageNumber, interval, newPdfFileName);
-                currentInvestor++;
+              
             }
         }
 
